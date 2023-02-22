1efaf512d3e27a2671e6f625708111c9e1e2598d
juanbono <juanbono94@gmail.com>
2023-02-22 17:33:14 -0300

    fix inverse

--- src/field.rs
+++ src/field.rs
@@ -1,7 +1,8 @@
+use rand::Rng;
 
 /// An implementation of field elements from F_(3 * 2**30 + 1).
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub(crate) struct FieldElement(usize);
+pub struct FieldElement(usize);
 
 impl FieldElement {
     pub fn new(value: usize) -> Self {
@@ -28,14 +29,14 @@ impl FieldElement {
 
     pub fn inverse(&self) -> Self {
         let (mut t, mut new_t) = (0, 1);
-        let (mut r, mut new_r) = (FieldElement::k_modulus(), self.0);
+        let (mut r, mut new_r) = (FieldElement::k_modulus() as i128, self.0 as i128);
         while new_r != 0 {
             let quotient = r / new_r;
             (t, new_t) = (new_t, t - (quotient * new_t));
             (r, new_r) = (new_r, r - quotient * new_r);
         }
         assert!(r == 1);
-        FieldElement::new(t)
+        t.into()
     }
 
     pub fn pow(&self, n: usize) -> Self {
@@ -64,6 +65,33 @@ impl FieldElement {
             }
         }
         h * self == FieldElement::one()
+    }
+
+    /// Generates a random FieldElement.
+    pub fn random_element(excluded_elements: &[FieldElement]) -> Self {
+        let mut rng = rand::thread_rng();
+        let mut fe = FieldElement::new(rng.gen::<usize>());
+        while excluded_elements.contains(&fe) {
+            fe = FieldElement::new(rng.gen::<usize>());
+        };
+        fe
+    }
+}
+
+impl From<usize> for FieldElement {
+    fn from(value: usize) -> Self {
+        FieldElement::new(value)
+    }
+}
+
+impl From<i128> for FieldElement {
+    fn from(value: i128) -> Self {
+        let value_mod_p = if value > 0 {
+            value % (FieldElement::k_modulus() as i128)
+        } else {
+            value + FieldElement::k_modulus() as i128
+        };
+        FieldElement::new(value_mod_p.try_into().unwrap())
     }
 }
 
@@ -128,8 +156,8 @@ impl std::ops::Sub for FieldElement {
     type Output = FieldElement;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        // TODO: check that this doesn't break.
-        FieldElement::new(self.0 - rhs.0)
+        let sub_result = self.0 as i128 - rhs.0 as i128;
+        sub_result.into()
     }
 }
 
@@ -162,5 +190,35 @@ impl std::ops::Neg for FieldElement {
 
     fn neg(self) -> Self::Output {
         FieldElement::zero() - self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::FieldElement;
+
+    #[test]
+    fn inverse_test() {
+        let x = FieldElement::new(2);
+        let x_inv = x.inverse();
+    
+        assert_eq!(FieldElement::one(), x * x_inv)
+    }
+
+    #[test]
+    fn test_field_wrap() {
+        // Check pow, mul, and the modular operations
+        let t = FieldElement(2).pow(30) * FieldElement(3) + FieldElement::one();
+        assert!(t == FieldElement::zero())
+    }
+    
+    #[test]
+    fn test_field_div() {
+        for _ in 1..10000 {
+        let t = FieldElement::random_element(&[FieldElement::zero()]);
+        let t_inv = FieldElement::one() / t;
+        assert!(t_inv == t.inverse());
+        assert!(t_inv * t == FieldElement::one());
+        }
     }
 }
